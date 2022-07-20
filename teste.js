80651fa7708126d7624d69905c703c81b8f9a9ca
felipe brito <felipe_delmondes@hotmail.com>
2022-07-20 10:39:13 -0300

    Testes iniciais

---
+++ teste.js
@@ -0,0 +1,23 @@
+//Comentário de linha
+//var y = 0;
+
+/*
+    Comentário de bloco
+*/
+
+//var preco = 2;
+var desconto = 0.2;
+//Constantes não podem ser manipuladas. As boas práticas pedem elas em Maísculas.
+const PRECO = 2; 
+var valorTotal = PRECO - desconto;
+
+
+//Declaração de funções
+function soma(a, b){
+    return a+b;
+}
+
+//Chamar função
+var soma = soma(1,2);
+console.log(soma);
+
